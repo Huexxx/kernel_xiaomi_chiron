0d9dfc770ab9b574cd85c111488f3bdc76a8dc20
Huexxx <huexxx@gmail.com>
2024-10-07 17:07:15 +0200

    syscall: Increase bpf fake uname to 5.8.18

--- kernel/sys.c
+++ kernel/sys.c
@@ -1159,7 +1159,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
-		strcpy(tmp.release, "5.4.284");
+		strcpy(tmp.release, "5.8.18");
 		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
